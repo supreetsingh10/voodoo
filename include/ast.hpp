d7e95e0081f0cca86d297b8c7db866e2bb20c59c
Supreet <singh.supreet14@gmail.com>
2024-07-09 20:46:18 +0530

    Implemented some parts of ast node.

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,40 +1,101 @@
-#include <string> 
+#pragma once
+#include <string>
 
+struct Node {
+    Node() {}
+    ~Node() {}
+};
 
-// TODO Empty for now. 
-enum ExprType {
+// Forward declaration 
+struct TypeNode;
+
+struct DeclarationNode : public Node {
+    DeclarationNode() {}
+    virtual ~DeclarationNode() {}
+
+    TypeNode* m_type;
+    DeclarationNode *next; 
+};
+
+enum Types {
+    TYPE_VOID,
+    TYPE_INT,
+    TYPE_FLOAT,
+    TYPE_CHAR,
+    TYPE_STRING,
+    TYPE_FUNCTION,
+};
+
+struct TypeNode; 
+struct ParamNode: public Node {
+    ParamNode() {}
+    virtual ~ParamNode() {}
+
+    std::string name;
+    TypeNode* m_param_type;
+    ParamNode* next;
 }; 
 
-class ExprNode {
-    public:
-        ExprNode(); 
-        ~ExprNode(); 
+struct TypeNode: public Node {
+    TypeNode() {}
+    virtual ~TypeNode() {}
 
-        ExprType m_etype; 
-        ExprNode* m_pright, *m_pleft; 
-        int m_intvalue; 
+    Types m_etype;
+    TypeNode* m_subtype;
 };
 
-class DeclarationNode {
-    public:
-        DeclarationNode(); 
-        ~DeclarationNode(); 
-
-        std::string m_sname; 
-        ExprNode* m_pvalue; 
-        DeclarationNode* m_pnext; 
-};
-
-// TODO To add more statement types. 
 enum StatementType {
     STMT_DECL,
+    STMT_EXPR,
+    STMT_IF,
+    STMT_ELSE_IF,
+    STMT_ELSE,
+    STMT_IF_ELSE,
+    STMT_FOR,
+    STMT_RETURN,
+    STMT_BLOCK,
 };
 
-class StatementNode {
-    public: 
-        StatementNode(); 
-        ~StatementNode(); 
+struct ExpressionNode; 
+struct StatementNode: public Node {
+    // Constuctors
+    StatementNode() {}
+    StatementNode(
+            DeclarationNode* decl_node,
+            ExpressionNode* init_expr,
+            ExpressionNode* expr,
+            ExpressionNode* next_expr,
+            StatementNode* body, 
+            StatementNode* else_body
+            ): 
+        m_stmt_decl(decl_node),
+        m_init_expr(init_expr),
+        m_expr(expr),
+        m_next_expr(next_expr),
+        m_body(body),
+        m_else_body(else_body)
+    {}
 
-        StatementType m_eType; 
+    virtual ~StatementNode() {}
+
+    StatementType m_stmt_type;
+    // Member objects
+    DeclarationNode* m_stmt_decl;
+    ExpressionNode* m_init_expr, *m_expr, *m_next_expr;
+    StatementNode* m_body, *m_else_body;
+    StatementNode* next;
 };
 
+
+enum ExpressionType {
+    EXPR_ASSIGN,
+    EXPR_ADD,
+    EXPR_COLON,
+};
+
+
+// TODO to set up expressions.
+struct ExpressionNode: public Node {
+    ExpressionNode() {}
+    virtual ~ExpressionNode() {}
+};
--- include/parser.hpp
+++ include/parser.hpp
@@ -1,17 +1,28 @@
 #pragma once
 
+#include <stack>
 #include <vector>
 #include "./token.hpp"
 
+
 class Parser {
-    public:
-        void parse_tokens(std::vector<Token*> tokens);
-        Token* scan_token(); 
-        Token* scan_token_at_index(int index); 
-        // this gets the next token
-        Token* peek_token(); 
+    public: 
+        Parser(); 
+        ~Parser(); 
+
+        Token* get_next(); 
+        Token* get_current(); 
+
+        void set_input_stream(const std::vector<Token*>& input_stream); 
+
+        bool parse(); 
+        bool parse_fn(Token* current_token);
 
     private: 
-        std::vector<Token*> m_vpTokens; 
-        int m_token_number; 
+        bool parse(Token* current_token); 
+
+        int m_Index; 
+        std::vector<Token*> m_vpInputTokens; 
+
+        std::stack<int> m_sBlockCounter;
 };
