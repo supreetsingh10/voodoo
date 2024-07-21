abbf68dab8e3dd35d84c98f2832b7af64cae5560
Supreet <singh.supreet14@gmail.com>
2024-07-21 14:12:25 +0530

    Added return type validator, but this has to be refactored in the future.

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,4 +1,23 @@
+#include "../include/ast.hpp"
+#include <map>
+#include <utility>
 
+ReturnType TypeNode::valid_return_type(const std::string& ret_type) {
+    std::map<std::string, ReturnType> m_RetTypes;
+    m_RetTypes.insert(std::make_pair("void", TYPE_VOID));
+    m_RetTypes.insert(std::make_pair("string", TYPE_STRING));
+    m_RetTypes.insert(std::make_pair("char", TYPE_CHAR));
+    m_RetTypes.insert(std::make_pair("int", TYPE_INT));
+    m_RetTypes.insert(std::make_pair("float", TYPE_FLOAT));
+
+    auto val = m_RetTypes.find(ret_type);
+
+    if (val == m_RetTypes.end()) {
+       return TYPE_INVALID; 
+    }
+
+    return val->second;
+}
 
 
 
