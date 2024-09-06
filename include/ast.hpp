930768a4f1d05169fb0af5a1d91b5d7cbe2c2646
Supreet <singh.supreet14@gmail.com>
2024-09-06 21:05:02 +0530

    Formatting changes and implementation of operator parsing.

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,194 +1,187 @@
 #pragma once
+#include "./operators.hpp"
 #include "datatypes.hpp"
 #include <cstdint>
 #include <string>
-#include "./operators.hpp"
 
 enum NodeType {
-    DECLARATION,
-    STATEMENT,
-    EXPRESSION,
+  DECLARATION,
+  STATEMENT,
+  EXPRESSION,
 };
 
 enum DeclarationType {
-    VARIABLE,
-    FUNCTION,
+  VARIABLE,
+  FUNCTION,
 };
 
 enum BlockType {
-    BLOCK_FOR_LOOP,
-    BLOCK_FUNC,
-    BLOCK_IF,
-    BLOCK_ELSE,
-    BLOCK_NORMAL,
-}; 
-
-struct Node
-{
-    Node() {}
-    Node(NodeType e): nodetype(e) {}
-    virtual ~Node() {}
-
-    NodeType nodetype;
-    static void describe_tree(Node* n); 
+  BLOCK_FOR_LOOP,
+  BLOCK_FUNC,
+  BLOCK_IF,
+  BLOCK_ELSE,
+  BLOCK_NORMAL,
 };
 
-// Forward declaration 
+struct Node {
+  Node() {}
+  Node(NodeType e) : nodetype(e) {}
+  virtual ~Node() {}
+
+  NodeType nodetype;
+  static void describe_tree(Node *n);
+};
+
+// Forward declaration
 struct TypeNode;
 struct ParamNode;
-struct StatementNode; 
+struct StatementNode;
 
-struct DeclarationNode: public Node 
-{
-    DeclarationNode(): Node(DECLARATION), m_param_node(nullptr), m_stmts(nullptr), next(nullptr) {}
-    virtual ~DeclarationNode();
+struct DeclarationNode : public Node {
+  DeclarationNode()
+      : Node(DECLARATION), m_param_node(nullptr), m_stmts(nullptr),
+        next(nullptr) {}
+  virtual ~DeclarationNode();
 
-    std::string decl_name;
-    DeclarationType decl_type;
-    // has the information about the return type.
-    TypeNode* m_type_node;
-    // Parameters. Can be none. 
-    ParamNode* m_param_node;
-    // Points to the code inside a declaration. 
-    // Becomes useful when we are working with function declarations.
-    StatementNode* m_stmts;
-    // points to next declaration 
-    DeclarationNode *next; 
+  std::string decl_name;
+  DeclarationType decl_type;
+  // has the information about the return type.
+  TypeNode *m_type_node;
+  // Parameters. Can be none.
+  ParamNode *m_param_node;
+  // Points to the code inside a declaration.
+  // Becomes useful when we are working with function declarations.
+  StatementNode *m_stmts;
+  // points to next declaration
+  DeclarationNode *next;
 
-    static void describe_decl(DeclarationNode* dn);
+  static void describe_decl(DeclarationNode *dn);
 };
 
-struct TypeNode; 
+struct TypeNode;
 // MAJOR REFACTOR
 // Use this to create params for the functions.
-// I can use this to always push to the end tail. 
-struct ParamNode: public Node 
-{
-    ParamNode(): m_param_type() {}
-    virtual ~ParamNode();
+// I can use this to always push to the end tail.
+struct ParamNode : public Node {
+  ParamNode() : m_param_type() {}
+  virtual ~ParamNode();
 
-    std::string name;
-    TypeNode* m_param_type;
-    // this will be pointing towards the next parameter. 
-    ParamNode* next;
+  std::string name;
+  TypeNode *m_param_type;
+  // this will be pointing towards the next parameter.
+  ParamNode *next;
 
-    static void describe_param(ParamNode* p);
-}; 
+  static void describe_param(ParamNode *p);
+};
 
 // Do not use this to create params for functions.
-struct TypeNode: public Node 
-{
-    TypeNode() {}
-    virtual ~TypeNode();
+struct TypeNode : public Node {
+  TypeNode() {}
+  virtual ~TypeNode();
 
-    DataType* m_data_node;
-    TypeNode* m_subtype;
-    static void describe_type(TypeNode* t); 
+  DataType *m_data_node;
+  TypeNode *m_subtype;
+  static void describe_type(TypeNode *t);
 
-    // MAJOR REFACTOR: Need to add move to a better check system for return type validation
-    // static ReturnType valid_return_type(const std::string& ret_type); 
-    static DataType* validate_token_type(const std::string& dat_type); 
-
+  // MAJOR REFACTOR: Need to add move to a better check system for return type
+  // validation static ReturnType valid_return_type(const std::string&
+  // ret_type);
+  static DataType *validate_token_type(const std::string &dat_type);
 };
 
 enum StatementType {
-    STMT_DECL,
-    STMT_EXPR,
-    STMT_IF,
-    STMT_ELSE_IF,
-    STMT_ELSE,
-    STMT_IF_ELSE,
-    STMT_FOR,
-    STMT_RETURN,
-    STMT_BLOCK,
+  STMT_DECL,
+  STMT_EXPR,
+  STMT_IF,
+  STMT_ELSE_IF,
+  STMT_ELSE,
+  STMT_IF_ELSE,
+  STMT_FOR,
+  STMT_RETURN,
+  STMT_BLOCK,
 };
 
-struct ExpressionNode; 
-struct StatementNode: public Node {
-    StatementNode():
-        m_stmt_decl(nullptr),
-        m_ifbody(nullptr),
-        m_else_body(nullptr),
-        m_sub_block(nullptr),
-        m_expr(nullptr),
-        m_init_expr(nullptr),
-        m_next_expr(nullptr),
-        next(nullptr)
-    {}
+struct ExpressionNode;
+struct StatementNode : public Node {
+  StatementNode()
+      : m_stmt_decl(nullptr), m_ifbody(nullptr), m_else_body(nullptr),
+        m_sub_block(nullptr), m_expr(nullptr), m_init_expr(nullptr),
+        m_next_expr(nullptr), next(nullptr) {}
 
-    virtual ~StatementNode();
+  virtual ~StatementNode();
 
-    StatementType m_stmt_type;
-    // Member objects
-    DeclarationNode* m_stmt_decl;
-    ExpressionNode* m_init_expr, *m_next_expr;
-    ExpressionNode* m_expr;
+  StatementType m_stmt_type;
+  // Member objects
+  DeclarationNode *m_stmt_decl;
+  ExpressionNode *m_init_expr, *m_next_expr;
+  ExpressionNode *m_expr;
 
-    StatementNode* m_ifbody, *m_else_body; 
-    // In case there are sub blocks in the statement.
-    StatementNode* m_sub_block;
-    // this points to the next statement in the block.
-    StatementNode* next;
+  StatementNode *m_ifbody, *m_else_body;
+  // In case there are sub blocks in the statement.
+  StatementNode *m_sub_block;
+  // this points to the next statement in the block.
+  StatementNode *next;
 
-    static void describe_stmt(StatementNode* s);
+  static void describe_stmt(StatementNode *s);
 };
 
-enum ExpressionOperator
-{
-    // Mathematical expression
-    // Binary expressions
-    EXPR_ADD,
-    EXPR_SUB,
-    EXPR_MUL,
-    EXPR_DIV,
-    EXPR_MOD,
+enum ExpressionOperator {
+  // Mathematical expression
+  // Binary expressions
+  EXPR_ADD,
+  EXPR_SUB,
+  EXPR_MUL,
+  EXPR_DIV,
+  EXPR_MOD,
 
-    // Binary
-    EXPR_ASSIGN,
-    // Boolean expressions
-    // All are binary expression
-    EXPR_EQUALS,
-    EXPR_GREATER_THAN,
-    EXPR_GREATER_EQUALS_TO,
-    EXPR_LESS_EQUALS_TO,
-    EXPR_NOT_EQUALS,
+  // Binary
+  EXPR_ASSIGN,
+  // Boolean expressions
+  // All are binary expression
+  EXPR_EQUALS,
+  EXPR_GREATER_THAN,
+  EXPR_GREATER_EQUALS_TO,
+  EXPR_LESS_EQUALS_TO,
+  EXPR_NOT_EQUALS,
 
+  // Bitwise
+  EXPR_AND,
+  EXPR_NOT,
+  EXPR_OR,
 
-    // Bitwise
-    EXPR_AND,
-    EXPR_NOT,
-    EXPR_OR,
-
-    EXPR_COLON,
-    EXPR_CALL,
-    EXPR_ARG,
-    EXPR_RETURN,
-    EXPR_SUBSCRIPT,
+  EXPR_COLON,
+  EXPR_CALL,
+  EXPR_ARG,
+  EXPR_RETURN,
+  EXPR_SUBSCRIPT,
 };
 
 enum ExprType {
-    EXPR_OPERATOR,
-    EXPR_OPERAND,
+  EXPR_OPERATOR,
+  EXPR_OPERAND,
 };
 
-struct ExpressionNode: public Node 
-{
-    ExpressionNode(): Node(EXPRESSION), left(nullptr), right(nullptr), m_id_exp_op(-1) {}
-    virtual ~ExpressionNode();
+struct ExpressionNode : public Node {
+  ExpressionNode()
+      : Node(EXPRESSION), left(nullptr), right(nullptr), m_id_exp_op(-1) {}
+  virtual ~ExpressionNode();
 
-    ExpressionNode* left, *right;
-    Operator m_expr_opr;
-    ExprType m_type;
+  ExpressionNode *left, *right;
+  Operator m_expr_opr;
+  ExprType m_type;
 
-    std::string m_expr_name;
-    DataType m_expr_data;
-    int m_id_exp_op;
-    static int last_added_op_node_id;
-    static ExpressionNode* last_added_node;
+  std::string m_expr_name;
+  DataType m_expr_data;
+  int m_id_exp_op;
+  const int get_expr_precedence() const { return m_expr_opr.get_precedence(); }
 
-    static ExpressionNode* get_expression_node_with_id(ExpressionNode* node_root, const int32_t node_id); 
-    static ExpressionNode* get_parent_node_for_op_expr(ExpressionNode* node_root, ExpressionNode* opr_node);
-    const int get_expr_precedence() const {
-        return m_expr_opr.get_precedence();
-    }
+  static int last_added_op_node_id;
+  static ExpressionNode *last_added_op_node;
+
+  static ExpressionNode *get_expression_node_with_id(ExpressionNode *node_root,
+                                                     const int32_t node_id,
+                                                     bool parent);
+  static ExpressionNode *
+  get_suitable_parent_node(ExpressionNode *node_root,
+                                       ExpressionNode *docker_node);
 };
--- src/ast.cpp
+++ src/ast.cpp
@@ -1,160 +1,155 @@
 #include "../include/ast.hpp"
 #include "../include/datatypes.hpp"
+#include <iostream>
 #include <map>
 #include <utility>
-#include <iostream>
 
+void Node::describe_tree(Node *n) {
+  if (!n)
+    return;
 
-void Node::describe_tree(Node *n) 
-{
-     if(!n)
-         return;
-
-     if (n->nodetype == DECLARATION) 
-         DeclarationNode::describe_decl(dynamic_cast<DeclarationNode*>(n));
-     else if(n->nodetype == STATEMENT)
-         StatementNode::describe_stmt(dynamic_cast<StatementNode*>(n));
+  if (n->nodetype == DECLARATION)
+    DeclarationNode::describe_decl(dynamic_cast<DeclarationNode *>(n));
+  else if (n->nodetype == STATEMENT)
+    StatementNode::describe_stmt(dynamic_cast<StatementNode *>(n));
 }
 
+void DeclarationNode::describe_decl(DeclarationNode *dn) {
+  if (!dn)
+    return;
 
-void DeclarationNode::describe_decl(DeclarationNode *dn) 
-{
-      if (!dn)
-         return;
+  std::cout << "Declaration name " << dn->decl_name << std::endl;
+  std::cout << "Declaration " << dn->decl_type << std::endl;
 
-      std::cout <<"Declaration name " << dn->decl_name << std::endl;
-      std::cout <<"Declaration " << dn->decl_type << std::endl;
+  TypeNode::describe_type(dn->m_type_node);
+  ParamNode::describe_param(dn->m_param_node);
+  StatementNode::describe_stmt(dn->m_stmts);
 
-      TypeNode::describe_type(dn->m_type_node);
-      ParamNode::describe_param(dn->m_param_node);
-      StatementNode::describe_stmt(dn->m_stmts);
-
-      describe_decl(dn->next);
+  describe_decl(dn->next);
 }
 
-DeclarationNode::~DeclarationNode() 
-{
-   if(m_type_node)
-      delete m_type_node;
+DeclarationNode::~DeclarationNode() {
+  if (m_type_node)
+    delete m_type_node;
 
-   if(m_stmts)
-      delete m_stmts;
+  if (m_stmts)
+    delete m_stmts;
 
-   if(m_param_node)
-      delete m_param_node;
+  if (m_param_node)
+    delete m_param_node;
 }
 
-ParamNode::~ParamNode() 
-{
-   if(m_param_type)
-      delete m_param_type;
+ParamNode::~ParamNode() {
+  if (m_param_type)
+    delete m_param_type;
 }
 
-TypeNode::~TypeNode() 
-{
-   if(m_subtype)
-      delete m_subtype;
+TypeNode::~TypeNode() {
+  if (m_subtype)
+    delete m_subtype;
 
-   if(m_data_node)
-        delete m_data_node;
+  if (m_data_node)
+    delete m_data_node;
 }
 
-void TypeNode::describe_type(TypeNode *t) 
-{
-      if(!t)
-         return;
+void TypeNode::describe_type(TypeNode *t) {
+  if (!t)
+    return;
 
-      std::cout << "Type node value " << t->m_data_node->m_data_type << " " << t->m_data_node->m_data_value << std::endl;
+  std::cout << "Type node value " << t->m_data_node->m_data_type << " "
+            << t->m_data_node->m_data_value << std::endl;
 }
 
-void ParamNode::describe_param(ParamNode *p) 
-{
-   if(!p)
-      return;
+void ParamNode::describe_param(ParamNode *p) {
+  if (!p)
+    return;
 
-   std::cout << "Param node " << p->name << std::endl; 
-   describe_param(p->next);
+  std::cout << "Param node " << p->name << std::endl;
+  describe_param(p->next);
 }
 
-StatementNode::~StatementNode() 
-{
-   if(m_stmt_decl)
-      delete m_stmt_decl;
-
+StatementNode::~StatementNode() {
+  if (m_stmt_decl)
+    delete m_stmt_decl;
 }
 
-void StatementNode::describe_stmt(StatementNode *s) 
-{
-      if(!s)
-         return;
+void StatementNode::describe_stmt(StatementNode *s) {
+  if (!s)
+    return;
 
-      DeclarationNode::describe_decl(s->m_stmt_decl);
+  DeclarationNode::describe_decl(s->m_stmt_decl);
 
-      std::cout << "This is a statement node " << std::endl;
-      describe_stmt(s->next);
+  std::cout << "This is a statement node " << std::endl;
+  describe_stmt(s->next);
 
-      std::cout << "Code block below " << std::endl;
-      describe_stmt(s->m_sub_block);
+  std::cout << "Code block below " << std::endl;
+  describe_stmt(s->m_sub_block);
 }
 
-DataType* TypeNode::validate_token_type(const std::string &dat_type) 
-{
-   // it can go through the parse tree to check if the declaration is user defined. 
-   // this is not optimal
+DataType *TypeNode::validate_token_type(const std::string &dat_type) {
+  // it can go through the parse tree to check if the declaration is user
+  // defined. this is not optimal
 
-   std::map<std::string, DATA_TYPE> m_RetTypes;
-   m_RetTypes.insert(std::make_pair("void", TYPE_VOID));
-   m_RetTypes.insert(std::make_pair("str", TYPE_STRING));
-   m_RetTypes.insert(std::make_pair("char", TYPE_CHAR));
-   m_RetTypes.insert(std::make_pair("int", TYPE_INT));
-   m_RetTypes.insert(std::make_pair("deci", TYPE_DECI));
+  std::map<std::string, DATA_TYPE> m_RetTypes;
+  m_RetTypes.insert(std::make_pair("void", TYPE_VOID));
+  m_RetTypes.insert(std::make_pair("str", TYPE_STRING));
+  m_RetTypes.insert(std::make_pair("char", TYPE_CHAR));
+  m_RetTypes.insert(std::make_pair("int", TYPE_INT));
+  m_RetTypes.insert(std::make_pair("deci", TYPE_DECI));
 
-    auto val = m_RetTypes.find(dat_type);
+  auto val = m_RetTypes.find(dat_type);
 
-   if (val == m_RetTypes.end()) 
-   {
-      return nullptr;
-   }
+  if (val == m_RetTypes.end()) {
+    return nullptr;
+  }
 
-   DataType *d = new DataType(val->second);
-   return d;
+  DataType *d = new DataType(val->second);
+  return d;
 }
 
-
-ExpressionNode::~ExpressionNode() 
-{
-   delete left;
-   delete right;
+ExpressionNode::~ExpressionNode() {
+  delete left;
+  delete right;
 }
 
 int ExpressionNode::last_added_op_node_id = 0;
-ExpressionNode* ExpressionNode::last_added_node = nullptr;
+ExpressionNode *ExpressionNode::last_added_op_node = nullptr;
 
-ExpressionNode* ExpressionNode::get_expression_node_with_id(ExpressionNode* expr_root, const int32_t node_id) {
-   ExpressionNode* local_node = nullptr; 
-   if(expr_root == nullptr)
-      return nullptr;
+ExpressionNode *ExpressionNode::get_expression_node_with_id(
+    ExpressionNode *expr_root, const int32_t node_id, bool parent_flag) {
+  if (expr_root == nullptr)
+    return nullptr;
 
-   if(expr_root->m_id_exp_op == node_id)
+  if (parent_flag) {
+    if (expr_root->left->m_id_exp_op == node_id ||
+        expr_root->right->m_id_exp_op == node_id)
       return expr_root;
+  } else {
+    if (expr_root->m_id_exp_op == node_id || expr_root->m_id_exp_op == node_id)
+      return expr_root;
+  }
 
+  ExpressionNode *local_node = nullptr;
+  local_node =
+      get_expression_node_with_id(expr_root->left, node_id, parent_flag);
+  if (local_node != nullptr)
+    return local_node;
 
-   local_node = get_expression_node_with_id(expr_root->left, node_id);
-   if(local_node != nullptr) 
-      return local_node;
+  local_node =
+      get_expression_node_with_id(expr_root->right, node_id, parent_flag);
+  if (local_node != nullptr)
+    return local_node;
 
-   local_node = get_expression_node_with_id(expr_root->right, node_id);
-   if(local_node != nullptr) 
-      return local_node;
-
-   // in case there is no node
-   return local_node;
+  // in case there is no node
+  return local_node;
 }
 
-ExpressionNode* get_parent_node_for_op_expr(ExpressionNode* node_root, ExpressionNode* opr_node) {
-   if(node_root == nullptr)
-      return nullptr;
+ExpressionNode *
+ExpressionNode::get_suitable_parent_node(ExpressionNode *node_root,
+                                         ExpressionNode *child_node) {
+   // taking the last expression node parent, which will help us decide how to proceed with the 
+  ExpressionNode* last_node_parent = get_expression_node_with_id(node_root, last_added_op_node_id, true);
+  ExpressionNode* last_node = (last_node_parent->left->m_id_exp_op == ExpressionNode::last_added_op_node_id) ? last_node_parent->left : last_node_parent->right;
 
-   // use the last added node id number here. We can figure out how to work with this.
-   return nullptr;
+   return (last_node->get_expr_precedence() >= child_node->get_expr_precedence())? last_node : last_node_parent;
 }
